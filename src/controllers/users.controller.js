03aa778e0b2f4a66816ed6038f700a41c474552a
pcasani <p&$$12G1t>
2020-07-21 14:45:43 +0200

    Set up and apply ESLint

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,51 +1,57 @@
 const axios = require('axios');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const geolib = require('geolib');
 
 require('dotenv').config();
+
 const apiUrl = process.env.API_URL;
 
 const getAllUsers = async () => new Promise((resolve, reject) => {
-    axios.get(`${apiUrl}/users`)
-        .then((response) => {
-            resolve(response.data);
-        })
-        .catch((error) => {
-            reject(error);
-        });
+  axios.get(`${apiUrl}/users`)
+    .then((response) => {
+      resolve(response.data);
+    })
+    .catch((error) => {
+      reject(error);
+    });
 });
 
 const getDistance = async (from, to) => {
-    const distanceInMetres = geolib.getDistance(from, to);
-    return geolib.convertDistance(distanceInMetres, "mi");
+  const distanceInMetres = geolib.getDistance(from, to);
+  return geolib.convertDistance(distanceInMetres, 'mi');
 };
 
 const getUsersFromCity = async (city) => new Promise((resolve, reject) => {
-    axios.get(`${apiUrl}/city/${city}/users`)
-        .then((response) => {
-            resolve(response.data);
-        })
-        .catch((error) => {
-            reject(error);
-        });
+  axios.get(`${apiUrl}/city/${city}/users`)
+    .then((response) => {
+      resolve(response.data);
+    })
+    .catch((error) => {
+      reject(error);
+    });
 });
 
-const getUsersNearLocation = async (location, radius) => {
-    const users = await getAllUsers();
-    const usersMatched = [];
+let getUsersNearLocation;
+// eslint-disable-next-line prefer-const
+getUsersNearLocation = async (location, radius) => {
+  const users = await getAllUsers();
+  const usersMatched = [];
 
-    for (const user of users) {
-        const between = await getDistance(location, user);
-        if (between <= radius) {
-            usersMatched.push(user);
-        }
+  // eslint-disable-next-line no-restricted-syntax
+  for (const user of users) {
+    // eslint-disable-next-line no-await-in-loop
+    const between = await getDistance(location, user);
+    if (between <= radius) {
+      usersMatched.push(user);
     }
+  }
 
-    return new Promise((resolve) => {
-        resolve(usersMatched);
-    });
+  return new Promise((resolve) => {
+    resolve(usersMatched);
+  });
 };
 
 module.exports = {
-    getUsersFromCity,
-    getUsersNearLocation
+  getUsersFromCity,
+  getUsersNearLocation,
 };
--- src/users.routes.js
+++ src/users.routes.js
@@ -1,23 +1,21 @@
-const UsersData = require('./controllers/users.controller');
 const _ = require('underscore');
+const UsersData = require('./controllers/users.controller');
 
 require('dotenv').config();
+
 const city = process.env.CITY;
 const distance = process.env.DISTANCE_FROM_CITY;
 
 exports.routesConfig = (app) => {
-
-    app.get(`/v1/users/${city}`, async (req, res) => {
-        try {
-            const usersFromCity = await UsersData.getUsersFromCity(city);
-            const usersNearLocation = await UsersData.getUsersNearLocation(city, distance);
-            const usersDistinct = _.union(usersFromCity, usersNearLocation);
-            return await res.json(usersDistinct);
-
-        } catch (error) {
-            res.status(error.response.status);
-            return res.send(error.message);
-        }
-    });
-
+  app.get(`/v1/users/${city}`, async (req, res) => {
+    try {
+      const usersFromCity = await UsersData.getUsersFromCity(city);
+      const usersNearLocation = await UsersData.getUsersNearLocation(city, distance);
+      const usersDistinct = _.union(usersFromCity, usersNearLocation);
+      return await res.json(usersDistinct);
+    } catch (error) {
+      res.status(error.response.status);
+      return res.send(error.message);
+    }
+  });
 };
